e5714c8568066f338959c562d94ea61942368fa8
Geoffrey Trees <gptrees@gmail.com>
2014-04-22 15:25:11 -0400

    Updated main file

--- GreenEngineConsole/Main.cs
+++ GreenEngineConsole/Main.cs
@@ -38,25 +38,25 @@ namespace GreenEngineConsole
 			material.ElasticModulus = 10000000.0;
 			
 			fem.MaterialList.Add (material);
-			
-			BeamElement beam1 = new BeamElement ();
-			beam1.Node1 = node1;
-			beam1.Node2 = node2;
-			beam1.Material = material;
-			
-			BeamElement beam2 = new BeamElement ();
-			beam2.Node1 = node2;
-			beam2.Node2 = node3;
-			beam2.Material = material;
-			
-			BeamElement beam3 = new BeamElement ();
-			beam3.Node1 = node3;
-			beam3.Node2 = node1;
-			beam3.Material = material;
-			
-			fem.ElementList.Add (beam1);
-			fem.ElementList.Add (beam2);
-			fem.ElementList.Add (beam3);
+
+            TrussElement truss1 = new TrussElement();
+            truss1.Node1 = node1;
+            truss1.Node2 = node2;
+            truss1.Material = material;
+
+            TrussElement truss2 = new TrussElement();
+            truss2.Node1 = node2;
+            truss2.Node2 = node3;
+            truss2.Material = material;
+
+            TrussElement truss3 = new TrussElement();
+            truss3.Node1 = node3;
+            truss3.Node2 = node1;
+            truss3.Material = material;
+
+            fem.ElementList.Add(truss1);
+            fem.ElementList.Add(truss2);
+            fem.ElementList.Add(truss3);
 
 			return fem;
 		}
