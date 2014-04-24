b55fb9bdb2c08ee2b8d1a9a60c04875b736026bf
Geoffrey Trees <gptrees@gmail.com>
2014-04-23 23:06:41 -0400

    Updated engine

--- GreenEngine/TestEngine.cs
+++ GreenEngine/TestEngine.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using GreenEngine.Model;
 using MathNet.Numerics;
 using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
 
 
 namespace GreenEngine
@@ -23,6 +24,7 @@ namespace GreenEngine
 		public void Analyze(FiniteElementModel fem)
 		{
 			HashSet<Tuple<int, DegreeType>> degreeSet = new HashSet<Tuple<int, DegreeType>>();
+            HashSet<Tuple<int, DegreeType>> degreeSetSolve = new HashSet<Tuple<int, DegreeType>>();
 
             // Determine degrees
 			// for now just do X and Y for truss * nodes
@@ -30,11 +32,21 @@ namespace GreenEngine
             {
                 degreeSet.Add(Tuple.Create<int, DegreeType>(node.NodeId, DegreeType.X));
                 degreeSet.Add(Tuple.Create<int, DegreeType>(node.NodeId, DegreeType.Y));
+                degreeSetSolve.Add(Tuple.Create<int, DegreeType>(node.NodeId, DegreeType.X));
+                degreeSetSolve.Add(Tuple.Create<int, DegreeType>(node.NodeId, DegreeType.Y));
             }
 
-            //Matrix<double> m;
-            //m.Solve(m);
+            // This should be moved up.  I am just hacking stuff together to learn a procedure
+            foreach (Node node in fem.NodeList)
+            {
+                if (node.Fx)
+                    degreeSetSolve.Remove(Tuple.Create<int, DegreeType>(node.NodeId, DegreeType.X));
 
+                if (node.Fy)
+                    degreeSetSolve.Remove(Tuple.Create<int, DegreeType>(node.NodeId, DegreeType.Y));
+            }
+           
+            Matrix<double> m = new SparseMatrix(degreeSetSolve.Count, degreeSetSolve.Count);
 		}
 
 
