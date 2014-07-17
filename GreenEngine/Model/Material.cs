669525671ab95ba086905d7c405a30cc77a0986f
Geoffrey Trees <gptrees@gmail.com>
2014-07-17 13:24:21 -0400

    Added copyright

--- GreenEngine/Model/Material.cs
+++ GreenEngine/Model/Material.cs
@@ -1,3 +1,11 @@
+/*******************************************************
+ * Copyright (C) 2014 Geoffrey Trees gptrees@gmail.com
+ * 
+ * This file is part of GreenEngine.
+ * 
+ * GreenEngine can not be copied and/or distributed without the express
+ * permission of Geoffrey Trees
+ *******************************************************/
 using System;
 
 namespace GreenEngine.Model
--- GreenEngineConsole/Tests/BeamTest2.cs
+++ GreenEngineConsole/Tests/BeamTest2.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*******************************************************
+ * Copyright (C) 2014 Geoffrey Trees gptrees@gmail.com
+ * 
+ * This file is part of GreenEngine.
+ * 
+ * GreenEngine can not be copied and/or distributed without the express
+ * permission of Geoffrey Trees
+ *******************************************************/
+using System;
 using GreenEngine.Model;
 using GreenEngine.Results;
 using GreenEngine;
